ca1877a95c27a6f40092a163183fb7485974abbd
Hung08 <hung.187pm20486@vanlanguni.vn>
2022-06-22 16:54:50 +0700

    Fluent API in Entity Framework Core.

--- MyWebAPIApp/Migrations/20220622061615_AddDonHangSchema.Designer.cs
+++ MyWebAPIApp/Migrations/20220622061615_AddDonHangSchema.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyWebAPIApp.Data;
 
 namespace MyWebAPIApp.Migrations
 {
     [DbContext(typeof(MyDBContext))]
-    partial class MyDBContextModelSnapshot : ModelSnapshot
+    [Migration("20220622061615_AddDonHangSchema")]
+    partial class AddDonHangSchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MyWebAPIApp/Migrations/20220622061615_AddDonHangSchema.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyWebAPIApp.Migrations
+{
+    public partial class AddDonHangSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DonHang",
+                columns: table => new
+                {
+                    MaDH = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NgayDat = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "getutcdate()"),
+                    NgayGiao = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    TinhTrangDonHang = table.Column<int>(type: "int", nullable: false),
+                    NguoiNhanHang = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    DiaChi = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SoDienThoai = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DonHang", x => x.MaDH);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ChiTietDonHang",
+                columns: table => new
+                {
+                    MaDH = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    MaHH = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SoLuong = table.Column<int>(type: "int", nullable: false),
+                    DonGia = table.Column<double>(type: "float", nullable: false),
+                    GiamGia = table.Column<byte>(type: "tinyint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChiTietDonHang", x => new { x.MaDH, x.MaHH });
+                    table.ForeignKey(
+                        name: "FK_CHITIETDONHANG_DONHANG",
+                        column: x => x.MaDH,
+                        principalTable: "DonHang",
+                        principalColumn: "MaDH",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CHITIETDONHANG_HANGHOA",
+                        column: x => x.MaHH,
+                        principalTable: "HangHoa",
+                        principalColumn: "MaHH",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChiTietDonHang_MaHH",
+                table: "ChiTietDonHang",
+                column: "MaHH");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChiTietDonHang");
+
+            migrationBuilder.DropTable(
+                name: "DonHang");
+        }
+    }
+}
